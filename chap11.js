a3454bd71c418957ab9371a653f507f3eca3aa23
xanthe270 <90760206+xanthe270@users.noreply.github.com>
2022-05-28 08:47:52 +0200

    Delete chap11.js

--- chap11.js
+++
@@ -1,7 +0,0 @@
-var people = ["Mary", "Bobby", "Judy", "Eddie", "Herbie", "Tony", "Bobby"];
-var otherPeople = ["Teddy", "Cathy", "G-berg", "Georgie"];
-people = people.concat(otherPeople)
-people = people.join(" # ")
-document.getElementById("peopleIKnow").innerHTML = people.toString();
-people.pop()
-people = people.push("Teddy")
